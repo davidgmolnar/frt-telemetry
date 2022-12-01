40a7183c040f4d996adcdaf31411c380ce51cb45
Molnár Dávid <molnardavidgergely@gmail.com>
2022-12-01 15:58:50 +0100

    Finished g-g plot

--- flutter_telemetry/lib/indicators/plot_2d.dart
+++ flutter_telemetry/lib/indicators/plot_2d.dart
@@ -40,12 +40,41 @@ class Plot2DState extends State<Plot2D>{
   }
 
   void update(){
+    bool updateX = false;
+    bool updateY = false;
     if(context.size!.width != width){
       setState(() {
         width = context.size!.width;
       });
     }
-    // TODO update data
+    Map<String, List<dynamic>?> temp_x = widget.getSignalValues(widget.subscribedSignals[0], true, false);
+    if(temp_x["values"]!.isNotEmpty && temp_x["values"]![0] != x.last && (temp_x["values"]![0] > -1 * widget.maxValue && temp_x["values"]![0] < widget.maxValue)){
+      updateX = true;
+    }
+    Map<String, List<dynamic>?> temp_y = widget.getSignalValues(widget.subscribedSignals[1], true, false);
+    if(temp_y["values"]!.isNotEmpty && temp_y["values"]![0] != y.last && (temp_y["values"]![0] > -1 * widget.maxValue && temp_y["values"]![0] < widget.maxValue)){
+      updateY = true;
+    }
+    if(updateX && !updateY){
+      setState(() {
+        x.removeAt(0);
+        x.add(temp_x["values"]![0]);
+      });
+    }
+    else if(updateX && updateY){
+      setState(() {
+        x.removeAt(0);
+        x.add(temp_x["values"]![0]);
+        y.removeAt(0);
+        y.add(temp_y["values"]![0]);
+      });
+    }
+    else if(!updateX && updateY){
+      setState(() {
+        y.removeAt(0);
+        y.add(temp_y["values"]![0]);
+      });
+    }
   }
   
   @override
@@ -53,60 +82,40 @@ class Plot2DState extends State<Plot2D>{
     double innersize = width * 0.75;
     return Container(
       constraints: const BoxConstraints(minHeight: 300, minWidth: 300),
-      child: InkWell(
-        onTap: (){
-          if(x.last >= 3){
-            x.removeAt(0);
-            x.add(-3);
-            y.removeAt(0);
-            y.add(-3);
-          }
-          else{
-            num temp = x.last;
-            x.removeAt(0);
-            x.add(temp + 0.5);
-            y.removeAt(0);
-            y.add(temp + 0.5);
-          }
-          setState(() {
-            
-          });
-        },
-        child: Stack(
-          alignment: AlignmentDirectional.bottomCenter,
-          children: [
-            Transform.translate(
-              offset: Offset(0, -width * 0.92),
-              child: Text(widget.title),
+      child: Stack(
+        alignment: AlignmentDirectional.bottomCenter,
+        children: [
+          Transform.translate(
+            offset: Offset(0, -width * 0.92),
+            child: Text(widget.title),
+          ),
+          Transform.translate(
+            offset: Offset(0, -width * 0.07),
+            child: Container(
+              decoration: BoxDecoration(border: Border.all(color: primaryColor, width: 1.0)),
+              width: innersize,
+              height: innersize,
             ),
-            Transform.translate(
-              offset: Offset(0, -width * 0.07),
-              child: Container(
-                decoration: BoxDecoration(border: Border.all(color: primaryColor, width: 1.0)),
-                width: innersize,
-                height: innersize,
-              ),
-            ),
+          ),
       
-            for(int i = 0; i < 4; i++)
-              Transform.translate(
-                offset: Offset(0, -innersize/2 - 10),
-                child: Transform.translate(
-                  offset: Offset.fromDirection(-pi/4 + i*pi/2, sqrt(2) * innersize/2 + 15),
-                  child: Text("(${xPrefix(i)}${widget.maxValue};${yPrefix(i)}${widget.maxValue})"),
-                )
-              ),
-              
-            for(int i = 0; i < 3; i++)
-              Transform.translate(
-                offset: Offset(
-                  1.0 * normalizeInbetween(x[i], -1 * widget.maxValue, widget.maxValue, -(innersize~/2).toInt(), (innersize~/2).toInt()),
-                  -1.0 * normalizeInbetween(y[i], -1 * widget.maxValue, widget.maxValue, 0, innersize.toInt())
-                ),
-                child: Text("•", style: TextStyle(fontSize: 30, color: primaryColor.withOpacity(0.2 + 0.8*(i/(2)))),),  
+          for(int i = 0; i < 4; i++)
+            Transform.translate(
+              offset: Offset(0, -innersize/2 - 10),
+              child: Transform.translate(
+                offset: Offset.fromDirection(-pi/4 + i*pi/2, sqrt(2) * innersize/2 + 15),
+                child: Text("(${xPrefix(i)}${widget.maxValue};${yPrefix(i)}${widget.maxValue})"),
               )
-          ],
-        ),
+            ),
+            
+          for(int i = 0; i < 3; i++)
+            Transform.translate(
+              offset: Offset(
+                1.0 * normalizeInbetween(x[i], -1 * widget.maxValue, widget.maxValue, -(innersize~/2).toInt(), (innersize~/2).toInt()),
+                -1.0 * normalizeInbetween(y[i], -1 * widget.maxValue, widget.maxValue, 0, innersize.toInt())
+              ),
+              child: Text("•", style: TextStyle(fontSize: 30, color: primaryColor.withOpacity(0.2 + 0.8*(i/(2)))),),  
+            )
+        ],
       ),
     );
   }
