edbf50dbbacce8871fdd8f4980e00433cb235bc6
Molnár Dávid <molnardavidgergely@gmail.com>
2023-07-15 12:55:23 +0200

    Changed lapdata signals, added logreplay

--- backend/TelemetryAPI/logreader.py
+++ backend/TelemetryAPI/logreader.py
@@ -1,10 +1,13 @@
+import socket
+from time import sleep
+
 from cantools.database import load_file, errors
 
-CAN1 = load_file('DBC_2023/CAN1.dbc')
-CAN2 = load_file('DBC_2023/CAN2.dbc')
+CAN1 = load_file('C:/Users/cupid/OneDrive/Asztali gép/DBC/DBC_2023/CAN1.dbc')
+CAN2 = load_file('C:/Users/cupid/OneDrive/Asztali gép/DBC/DBC_2023/CAN2.dbc')
 
 print_list = [
-    "VDC_Yaw_Control_P"
+    "HV_Cell_ID"
 ]
 
 
@@ -39,16 +42,45 @@ def run(log_path: str, convert: bool = False):
             decoded = dict()
             while offset < length - 10:
                 try:
-                    decoded.update(decode(payload[offset:offset+10]))
+                    decoded.update(decode(payload[offset:offset + 10]))
                     if convert:
-                        converted_file.write(payload[offset:offset+10] + timestamp.to_bytes(4, 'big'))
+                        converted_file.write(payload[offset:offset + 10] + timestamp.to_bytes(4, 'big'))
                     offset += 10
                 except KeyError:
                     offset += 1
             for key in print_list:
                 if key in decoded.keys():
-                    print(f"{bin(int(decoded[key] / 25)).rjust(10,' ')} - {int(decoded[key] / 25)} - {timestamp}")
+                    print(f"{int(decoded[key])} - {timestamp}")
+
+
+def replay(log_path: str):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)  # UDP
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    with open(log_path, 'rb') as file:
+        first = True
+        prev_timestamp = None
+        addr = ("255.255.255.255", 8998)
+        while True:
+            # format
+            #    uint32 -> lenght
+            #    length bytes -> payload
+            #    uint64 -> timestamp ms
+            length = int.from_bytes(file.read(4), 'big')
+            payload = file.read(length)
+            timestamp = int.from_bytes(file.read(8), 'big')
+            if length == 0 and timestamp == 0:
+                print('Finished')
+                break
+            if first:
+                first = False
+                prev_timestamp = timestamp
+                sock.sendto(payload, addr)
+            else:
+                sleep((timestamp - prev_timestamp) / 1000)
+                sock.sendto(payload, addr)
+                prev_timestamp = timestamp
 
 
 if __name__ == '__main__':
-    run("telemetry_log.bin", convert=True)
+    # run("C:/Users/cupid/Downloads/PACSONYI.bin", convert=False)
+    replay("C:/Users/cupid/Downloads/PACSONYI.bin")
